20dfc86d4fd385a38fd1f34fe936defb0b341f32
AbbyKuo <abbykuo8776@gmail.com>
2024-11-19 19:52:04 +1000

    completed findal steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import TodoItem from './components/TodoItem';
+import TodoForm from './components/TodoForm';
+import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -8,13 +10,21 @@ function App() {
     "Build a really cool todo app",
   ]);
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, text];
+    setTodos(newTodos);
+    };
+
   return (
-      <div>
-          <h1>My todo list</h1>
+      <div className='app'>
+          <h1 className='todo-list'>My todo list</h1>
           <TodoItem text="Finish plus project." />
           <TodoItem text="Feed Brownie yummy bons." />
           <TodoItem text="Be awesome and grateful." />
-          {todos.map()}
+          {todos.map((todo, index) => (
+              <TodoItem text={todo} key={index} />
+        ))}
+        <TodoForm addTodo={addTodo} />
     </div >
   );
 }
