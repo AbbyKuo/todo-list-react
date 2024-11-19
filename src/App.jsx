931ac30bab3854496f0b5dece76c4307de044e9c
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-11-19 19:11:35 +1000

    uptostep7

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import TodoItem from './components/TodoItem';
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Learn about React",
+    "Meet a friend for lunch",
+    "Build a really cool todo app",
+  ]);
+
   return (
       <div>
           <h1>My todo list</h1>
           <TodoItem text="Finish plus project." />
           <TodoItem text="Feed Brownie yummy bons." />
           <TodoItem text="Be awesome and grateful." />
-      </div>
+          {todos.map()}
+    </div >
   );
 }
 
